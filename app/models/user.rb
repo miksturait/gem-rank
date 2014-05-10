19dc222d154e3af43a202acad0ba78a798d9fdbe
tommybb <tdudzik@gmail.com>
2014-05-10 14:32:24 +0200

    relations_added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :examples
+  has_many :example_alts
 end
